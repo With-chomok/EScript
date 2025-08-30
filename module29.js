c45430da830c2ac09f71913e5a9475221dba14cb
Chomok Das <183892208+With-chomok@users.noreply.github.com>
2025-08-30 14:50:18 +0600

    distructing and freezing.

--- module29.js
+++ module29.js
@@ -77,9 +77,9 @@ const copyNum = [...numbers];
 const copyNumpush = [...copyNum];
 copyNumpush.push(2222, 3000, 5000);
 // console.log(copyNum);
-console.log(copyNumpush);
+// console.log(copyNumpush);
 let max = Math.max(...copyNumpush);
-console.log(max);
+// console.log(max);
 
 const objects = {
   name: "dipol",
@@ -97,3 +97,81 @@ const newObj = { ...objects, designation: "programmer", study: "Engineering" };
 
 // const bothPersons = { ...person2, ...person1 };
 // console.log(bothPersons);
+
+// freez of object or array etc delete create update anything don't wrking
+
+// seel of object --- no work  just upadte
+
+// distruct object
+const freezs = {
+  name: "chomok",
+  roll: 266487,
+  subss: "CST",
+  Ins: "MPI",
+};
+// console.log({freezs.keys})
+
+// const names = freezs.name;
+// const rolls = freezs.roll;
+// console.log(names);
+// console.log(roll);
+const { name, roll } = freezs;
+// console.log(name, roll);
+
+// const {subss, Ins} = freezs;
+// // console.log(subss, Ins)
+// const deletes = delete(name);
+// freezs.push("mango")
+
+// console.log(freezs);
+
+const names = Object.keys(freezs);
+const value = Object.values(freezs);
+const values = Object.keys(newObj)
+// console.log(values);
+const counsts = Object.values(newObj)
+// console.log(counsts);
+const entries = Object.entries(newObj, freezs);
+// console.log(entries);
+
+// console.log(value);
+// Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+
+// console.log(names);
+   let myObject = { name: "Alice", age: 30, city: "New York" };
+  //  Object.freeze(myObject);
+   
+   myObject.name = "dipol";
+   myObject.age = 20;
+   myObject.city = "BD"
+   delete myObject.age;
+   delete myObject.city;
+   delete myObject.name
+  //  const myObjects = Object.freeze(myObject)   
+  console.log(myObject);
+
+//   let myArray = [];
+// myArray.push(myObject);
+// console.log(myArray);
+
+  //  console.log(myObject);
+  //  const objSeel = {
+  //   name : "hasna",
+  //   bari: "kuuet",
+  //   kikoy: "biye korbe"
+  //  }
+  //  let seel = Object.seal(objSeel)
+
+  let myObj = { name: "Alice", age: 30, city: "New York" };
+  for(let cleanObj in myObj){
+
+    // const obj = cleanObj;
+
+    console.log(cleanObj);
+  }
+  let myarrr = [1,2,5,4,7,8,54,6,4,8,4,1,5,6,21,6,5,23,1,5465,54,6451,3213,654,6,1231,]
+  for (let arr of myarrr){
+
+    console.log(arr);
+  }
+  
